4bd420a5ba434f77441764586fe072f1c07c4ddb
wikupurwoadi <wiku1800016059@webmail.uad.ac.id>
2020-03-28 15:54:03 +0700

    First Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './halaman_utama.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tekweb1800016059';
+}
